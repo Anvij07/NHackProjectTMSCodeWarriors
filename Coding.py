1a6420da84738fd960f06ff56db26d500e3b8d8b
Anvij07 <168573436+Anvij07@users.noreply.github.com>
2024-04-30 22:02:48 +0530

    Adding True loop

--- Coding.py
+++ Coding.py
@@ -1,9 +1,11 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-stock=str(input("enter the stock you want a graph for,Some examples are- AAPL,AMZN,MSFT,GOOG")).upper()
-stock_information=yf.Ticker(stock)
-prices=stock_information.history(period='1y')
-prices=prices['High']
-plt.plot(prices, marker='.', linestyle='-', color='b', label='stock price')
-plt.show()
+while True:
+    stock=str(input("Enter the stock you want a graph for,Some examples are- AAPL,AMZN,MSFT,GOOG")).upper()
+    stock_information=yf.Ticker(stock)
+    prices=stock_information.history(period='1y')
+    prices=prices['High']
+    plt.plot(prices, marker='.', linestyle='-', color='b', label='stock price')
+    plt.show()
+   
